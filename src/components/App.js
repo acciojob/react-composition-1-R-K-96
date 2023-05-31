3167efc71ac9f88b733b3210a9ccd4cf0a9cf32e
R-K-96 <kashmirehrithik@gmail.com>
2023-05-31 13:32:41 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
-
-import React from "react";
-import './../styles/App.css';
-import Tabs from "./Tabs";
+import React from 'react';
+import Tabs from './Tabs';
 
 const App = () => {
-  const tabs = ["Tab 1","Tab 2","Tab 3"]
+  const tabs = [
+    {
+      title: 'Tab 1',
+      content: 'This is the content for Tab 1',
+    },
+    {
+      title: 'Tab 2',
+      content: 'This is the content for Tab 2',
+    },
+    {
+      title: 'Tab 3',
+      content: 'This is the content for Tab 3',
+    },
+  ];
+
   return (
     <div>
-        <Tabs tabs={tabs}/>
+      <h1>Tab Example</h1>
+      <Tabs tabs={tabs} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,29 +1,28 @@
+import React, { useState } from 'react';
 
-import React,{useState} from "react";
+const Tabs = ({ tabs }) => {
+  const [activeTab, setActiveTab] = useState(0);
 
-const Tabs = ({tabs}) =>{
+  const handleClick = (index) => {
+    setActiveTab(index);
+  };
 
-    const [activeTab,setActiveTabs] = useState(0);
-
-    const handleClick = (index) =>{
-        setActiveTabs(index);
-    };
-
-    return(
-        <div>
-            <ul>
-                {
-                    tabs.map((tab,index)=>(
-                        <li key={index}
-                        onClick={()=>handleClick(index)}>
-                            {tab}
-                        </li>
-                    ))
-                }
-            </ul>
-            <div>This is content of {tabs[activeTab]}</div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <ul>
+        {tabs.map((tab, index) => (
+          <li
+            key={index}
+            onClick={() => handleClick(index)}
+            style={{ cursor: 'pointer' }}
+          >
+            {tab.title}
+          </li>
+        ))}
+      </ul>
+      <div>{tabs[activeTab].content}</div>
+    </div>
+  );
+};
 
 export default Tabs;
